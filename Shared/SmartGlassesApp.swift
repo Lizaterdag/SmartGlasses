5c3ba2a1f4fcad29dcdf4fcfeb82cd16b1d7fe88
Liza Darwesh <liza.darwesh@hva.nl>
2022-07-21 12:28:09 +0200

    Initial Commit

---
+++ Shared/SmartGlassesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartGlassesApp.swift
+//  Shared
+//
+//  Created by Liza Darwesh on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SmartGlassesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
